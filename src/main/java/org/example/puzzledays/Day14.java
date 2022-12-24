c1906955f05b3d843ea82b25b8bea3752545b1e1
Annelotte Fellinger <annelottef@gmail.com>
2022-12-24 10:02:13 +0100

    Day 14 setup

---
+++ src/main/java/org/example/puzzledays/Day14.java
@@ -0,0 +1,27 @@
+package org.example.puzzledays;
+
+import org.example.utils.PuzzleDay;
+
+import java.util.ArrayList;
+
+/*
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
+*/
+
+public class Day14 extends PuzzleDay {
+    public Day14() {
+        super(false, false, true);
+    }
+
+    @Override
+    public long getSolutionPartOne(ArrayList<String> input) {
+        System.out.println(Character.getName(10));
+        return 0;
+    }
+
+    @Override
+    public long getSolutionPartTwo(ArrayList<String> input) {
+        return 0;
+    }
+}
