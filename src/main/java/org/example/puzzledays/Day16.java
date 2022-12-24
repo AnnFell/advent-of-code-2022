6e6f107ed88ef38e33d65d8c96ba1fcabef9ec34
Annelotte Fellinger <annelottef@gmail.com>
2022-12-24 10:33:52 +0100

    Puzzle data so far

---
+++ src/main/java/org/example/puzzledays/Day16.java
@@ -0,0 +1,21 @@
+package org.example.puzzledays;
+
+import org.example.utils.PuzzleDay;
+
+import java.util.ArrayList;
+
+public class Day16 extends PuzzleDay {
+    public Day16() {
+        super(false, false, true);
+    }
+
+    @Override
+    public long getSolutionPartOne(ArrayList<String> input) {
+        return 0;
+    }
+
+    @Override
+    public long getSolutionPartTwo(ArrayList<String> input) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/puzzledays/Day19.java
@@ -0,0 +1,21 @@
+package org.example.puzzledays;
+
+import org.example.utils.PuzzleDay;
+
+import java.util.ArrayList;
+
+public class Day19 extends PuzzleDay {
+    public Day19() {
+        super(false, false, true);
+    }
+
+    @Override
+    public long getSolutionPartOne(ArrayList<String> input) {
+        return 0;
+    }
+
+    @Override
+    public long getSolutionPartTwo(ArrayList<String> input) {
+        return 0;
+    }
+}
